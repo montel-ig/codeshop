12a4ea6d19972969cc6822a3c18a5255891d0acf
tommi <zenbum@users.noreply.github.com>
2015-02-01 09:28:12 +0200

    Closes #2.

--- extranet/models.py
+++ extranet/models.py
@@ -3,3 +3,8 @@ from django.db import models
 
 class Customer(models.Model):
     name = models.CharField(max_length=100)
+
+
+class Need(models.Model):
+    customer = models.ForeignKey(Customer)
+    name = models.CharField(max_length=200)
