4465a1b22fed7acf04a9604d3e1060e9c3ed5c25
tommi <zenbum@users.noreply.github.com>
2015-02-16 13:58:50 +0200

    Add links to Github issues from weekly meeting view.

--- extranet/templatetags/report_tags.py
+++ extranet/templatetags/report_tags.py
@@ -7,11 +7,13 @@ register = template.Library()
 @register.inclusion_tag('extranet/show_hours_report.html')
 def show_hours_report(report, show_timenav=True, show_header=True,
                       show_coder_related_to_hours=False,
-                      show_project_for_other_hours=True):
+                      show_project_for_other_hours=True,
+                      link_title_to_github=False):
     return {
         'report': report,
         'show_timenav': show_timenav,
         'show_header': show_header,
         'show_coder_related_to_hours': show_coder_related_to_hours,
         'show_project_for_other_hours':show_project_for_other_hours,
+        'link_title_to_github': link_title_to_github,
     }
